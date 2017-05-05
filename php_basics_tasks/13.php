93ca87bf1e4a2f8fad1bf831edefd0363922cec8
eartempro <eartempro@gmail.com>
2017-05-05 11:12:57 +0300

    Create 13.php

---
+++ php_basics_tasks/13.php
@@ -0,0 +1,11 @@
+<?php 
+// Длина учаска kolometers
+$s = 60;
+// time i hours
+$t = 1;
+
+// Скорорсть автомобиля в метрах в секунду
+echo "Speed in Km/h " . ($s/$t);
+echo "<br>";
+// время движения в часах
+echo "Speed in m/s " . ($s/($t*3.6));
