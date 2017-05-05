1d9841fe3ac7ba6b18d1c6aed9d0ce1d14320a0c
eartempro <eartempro@gmail.com>
2017-05-05 16:18:04 +0300

    Create 23.php

---
+++ php_basics_tasks/23.php
@@ -0,0 +1,9 @@
+<?php 
+$text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt";
+
+?>
+<?=$text ?>
+<br>
+<?php echo($text); ?>
+<br>
+<?php print($text); ?>
