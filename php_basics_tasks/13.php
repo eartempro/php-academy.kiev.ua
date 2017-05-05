2b64ede55a33ef23e6f34e367a7de5baf5dce645
eartempro <eartempro@gmail.com>
2017-05-05 15:17:32 +0300

    Update 13.php

--- php_basics_tasks/13.php
+++ php_basics_tasks/13.php
@@ -1,11 +1,13 @@
 <?php 
-// Длина учаска kolometers
-$s = 60;
+// set vars
+$s = 60; // distance of moving in kilometres
 // time i hours
-$t = 1;
+$t = 1; // time of moving in hours
 
-// Скорорсть автомобиля в метрах в секунду
-echo "Speed in Km/h " . ($s/$t);
-echo "<br>";
+// 
+$speed_km_per_h = $s / $t;
+$speed_m_per_s = $speed_km_per_h * 2.777777778;// km/h ==> (1000 * m) / (3600 * s) => 0.27777778(m/s)
+echo "Speed in Km/h " .$speed_km_per_h;
+echo " ";
 // время движения в часах
-echo "Speed in m/s " . ($s/($t*3.6));
+echo "Speed in m/s " . $speed_m_per_s;
